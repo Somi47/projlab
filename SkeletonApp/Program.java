283c574c719056a1a407f40335a76ba0b60871b9
hollowfizz <hollowfizz@hotmail.hu>
2018-03-18 22:52:01 +0100

    menü v1.0

--- SkeletonApp/Program.java
+++ SkeletonApp/Program.java
@@ -6,7 +6,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import Utility.FunctionLogger;
 
-public class Program {
+public class Program 
+{	
+	static Warehouse warehouse;
+	static Worker worker1;
+	static Worker worker2;
+	static Box box1;
+	static Box box2;
+	static Grinder grinder1;
+	static Pit pit1;
+	static Switch switch1;
+	static Wall wall1;
+	static Floor floor1;
+	static Floor floor2;
+	
+	Program()
+	{
+		warehouse = new Warehouse();
+		worker1 = new Worker();
+		worker2 = new Worker();	
+		floor1 = new Floor();
+		floor2 = new Floor();
+		box1 = new Box();
+		box2 = new Box();
+		grinder1 = new Grinder();
+		pit1 = new Pit();
+		switch1 = new Switch();
+		wall1 = new Wall();
+	}
+
 	
 	public static void cls() throws IOException 
 	{  
@@ -21,278 +49,269 @@ public class Program {
 		}
 	}
 	
+
+	
+	public static void InitMenu(String input) throws IOException
+	{			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inner_input;
+		
+		switch(input)
+		{
+		case "1":
+			System.out.println("Pálya betöltése");
+			// Load map?
+			warehouse.Initialize();
+			break;
+		case "2":
+			System.out.println("Játékosok számának beállítása");
+			// Set worker number
+			System.out.println("Játékosok száma: (1-2)");
+			inner_input = br.readLine();
+			
+			if(inner_input.equals("1"))
+			{
+				System.out.println("Játékosok száma: 1");					
+				warehouse.SetWorkerNumber(1);
+				break;
+			}					
+			if(inner_input.equals("2"))
+			{
+				System.out.println("Játékosok száma: 2");
+				warehouse.SetWorkerNumber(2);
+				break;
+			}					
+			else
+			{
+				System.out.println("Hibás bemenet!");
+				break;
+			}					
+		case "3":
+			System.out.println("Játékos(ok) nevének beállítása");
+			// Set worker's name
+			if(warehouse.GetWorkerNumber() == 1)
+			{
+				System.out.println("1. játékos neve: ");
+				inner_input = br.readLine();
+				worker1.SetName(inner_input);												
+			}
+			if(warehouse.GetWorkerNumber() == 2)
+			{
+				System.out.println("1. játékos neve: ");
+				inner_input = br.readLine();
+				worker1.SetName(inner_input);
+				System.out.println("2. játékos neve: ");
+				inner_input = br.readLine();
+				worker2.SetName(inner_input);
+			}
+			break;
+		default:
+			System.out.println("Hibás bemenet!");
+		}
+	}
+	
+	public static void MainMenu()
+	{
+		System.out.println("1. Játék inicializálása");
+		System.out.println("2. Munkás mozgatása");
+		System.out.println("3. Játék befejezése");
+		System.out.println("4. Kilépés");
+	}
+	
+	
+	public static void WorkerMoveMenu(String input) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inner_input;	
+		
+		if(input.equalsIgnoreCase("t"))
+		{
+			System.out.println("1. Fallal ütközik");
+			System.out.println("2. Ládával ütközik");
+			System.out.println("3. Lyukkal ütközik");
+			System.out.println("4. Munkással ütközik");
+			System.out.println("5. Kapcsolóval ütközik");
+			inner_input = br.readLine();
+			switch(inner_input)
+			{
+			case "1":
+				System.out.println("Fallal ütközik");						
+				
+				//Worker hit wall
+				
+				floor1.Add(worker1);
+				worker1.SetField(floor1);
+				
+				floor1.SetNeighbor(Direction.Up, floor2);
+				floor2.SetNeighbor(Direction.Down, floor1);
+				
+				floor2.Add(wall1);
+				wall1.SetField(floor2);
+				
+				wall1.HitBy(Direction.Right, worker1);
+				worker1.Move(Direction.Up);
+				
+				break;
+			case "2":
+				System.out.println("Ládával ütközik");
+				// Worker hit box
+				floor1.Add(worker1);
+				worker1.SetField(floor1);
+				
+				floor1.SetNeighbor(Direction.Up, floor2);
+				floor2.SetNeighbor(Direction.Down, floor1);
+				
+				floor2.Add(box1);
+				wall1.SetField(floor2);
+				
+				wall1.HitBy(Direction.Right, worker1);
+				worker1.Move(Direction.Up);
+				
+				System.out.println("El tudja tolni? (T/F)");
+				inner_input = br.readLine();
+				if(inner_input.equalsIgnoreCase("t"))
+				{
+					// Worker move box
+					System.out.println("1. Másik doboznak tolja");
+					System.out.println("2. Munkásnak tolja");
+					System.out.println("3. Falnak tolja");
+					System.out.println("4. Lyukba tolja");
+					System.out.println("5. Darálóba tolja");
+					System.out.println("6. Kapcsolóra tolja");
+					inner_input = br.readLine();
+					switch(inner_input)
+					{
+					case "1":
+						System.out.println("Másik doboznak tolja");
+						// Worker move box hit box
+						break;
+					case "2":
+						System.out.println("Munkásnak tolja");
+						// Worker move box hit worker 
+						break;
+					case "3":
+						System.out.println("Falnak tolja");
+						// Worker move box hit wall
+						break;
+					case "4":
+						System.out.println("Lyukba tolja");
+						// Worker move box hit pit
+						break;
+					case "5":
+						System.out.println("Darálóba tolja");
+						// Worker move box hit grinder
+						System.out.println("Szerez pontot? (T/F)");
+						inner_input = br.readLine();
+						if(inner_input.equalsIgnoreCase("t"))
+						{
+							System.out.println("Pontot szerez");
+							// points++
+							worker1.AddPoint();									
+							break;
+						}
+						if(input.equalsIgnoreCase("f"))
+						{
+							System.out.println("Nem szerez pontot");
+							break;
+						}
+						break;
+					case "6":
+						System.out.println("Kapcsolóra tolja");
+						// Worker move box hit switch
+						break;									
+					}					
+				}
+				if(inner_input.equalsIgnoreCase("f"))
+				{
+					System.out.println("Nem tudja eltolni");
+					break;
+				}						
+				break;
+			case "3":
+				System.out.println("Lyukkal ütközik");
+				//Worker hit pit					
+				break;
+			case "4":
+				System.out.println("Munkással ütközik");
+				//Worker hit worker			
+				break;
+			case "5":
+				System.out.println("Kapcsolóval ütközik");					
+				// Worker hit switch																					
+				break;
+			default:
+				System.out.println("Hibás bemenet!");						
+			}
+		}
+		if(input.equalsIgnoreCase("f"))
+		{
+			System.out.println("Nem ütközik semmivel");
+		}																
+	}
+	
+	public static void EndGameMenu(String input)
+	{	
+		switch(input)
+		{
+		case "1":
+			System.out.println("Játék vége");
+			// Call EndGame
+			warehouse.EndGame();
+			break;
+		case "2":
+			System.out.println("Eredmények kijelzése");
+			// Show points
+			if(warehouse.GetWorkerNumber() > 1)
+			{
+				System.out.println("1. játékos pontjai: " + worker1.GetPoints());
+				System.out.println("2. játékos pontjai: " + worker2.GetPoints());
+				break;
+			}
+			else
+			{
+				System.out.println("1. játékos pontjai: " + worker1.GetPoints());						
+				break;
+			}
+		case "3":
+			System.out.println("Új játék");
+			// New game
+			break;
+		default:
+			System.out.println("Hibás bemenet!");
+		}								
+	}
+	
 	public static void main(String[] args) throws IOException 
 	{
-		//Objektumok létrehozása
-		Warehouse warehouse = new Warehouse();
-		Worker worker1 = new Worker();
-		Worker worker2 = new Worker();	
-		Floor floor1 = new Floor();
-		Floor floor2 = new Floor();
-		Box box1 = new Box();
-		Box box2 = new Box();
-		Grinder grinder1 = new Grinder();
-		Pit pit1 = new Pit();
-		Switch switch1 = new Switch();
-		Wall wall1 = new Wall();
-		
-		//
+		Program p = new Program();		
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input;
+		
 		while(true)
-		{
-			nl(3);
-			System.out.println("1. Játék inicializálása");
-			System.out.println("2. Munkás mozgatása");
-			System.out.println("3. Játék befejezése");
-			System.out.println("4. Kilépés");
+		{			
+			MainMenu();
 			input = br.readLine();
 			switch(input)
 			{
-			case "1":
+			case "1":				
 				System.out.println("1. Pálya betöltése");
 				System.out.println("2. Játékosok számának beállítása");
 				System.out.println("3. Játékos(ok) nevének beállítása");
 				input = br.readLine();
-				switch(input)
-				{
-				case "1":
-					System.out.println("Pálya betöltése");
-					// Load map?
-					warehouse.Initialize();
-					break;
-				case "2":
-					System.out.println("Játékosok számának beállítása");
-					// Set worker number
-					System.out.println("Játékosok száma: (1-2)");
-					input = br.readLine();
-					
-					if(input.equals("1"))
-					{
-						System.out.println("Játékosok száma: 1");					
-						warehouse.SetWorkerNumber(1);
-						break;
-					}					
-					if(input.equals("2"))
-					{
-						System.out.println("Játékosok száma: 2");
-						warehouse.SetWorkerNumber(2);
-						break;
-					}					
-					else
-					{
-						System.out.println("Hibás bemenet!");
-						break;
-					}					
-				case "3":
-					System.out.println("Játékos(ok) nevének beállítása");
-					// Set worker's name
-					if(warehouse.GetWorkerNumber() == 1)
-					{
-						System.out.println("1. játékos neve: ");
-						input = br.readLine();
-						worker1.SetName(input);												
-					}
-					if(warehouse.GetWorkerNumber() == 2)
-					{
-						System.out.println("1. játékos neve: ");
-						input = br.readLine();
-						worker1.SetName(input);
-						System.out.println("2. játékos neve: ");
-						input = br.readLine();
-						worker2.SetName(input);
-					}
-					break;
-				default:
-					System.out.println("Hibás bemenet!");
-				}
+				InitMenu(input);
 				break;
 			case "2":
 				System.out.println("Ütközik valamivel? (T/F)");
 				input = br.readLine();
-				if(input.equalsIgnoreCase("t"))
-				{
-					System.out.println("1. Fallal ütközik");
-					System.out.println("2. Ládával ütközik");
-					System.out.println("3. Lyukkal ütközik");
-					System.out.println("4. Munkással ütközik");
-					System.out.println("5. Kapcsolóval ütközik");
-					input = br.readLine();
-					switch(input)
-					{
-					case "1":
-						System.out.println("Fallal ütközik? (T/F)");
-						input = br.readLine();
-						if(input.equalsIgnoreCase("t"))
-						{
-							//Worker hit wall fgv							
-							wall1.HitBy(Direction.Right, worker1);
-						}
-						if(input.equalsIgnoreCase("f"))
-						{
-							System.out.println("Nem ütközik fallal");
-							break;
-						}
-						break;
-					case "2":
-						System.out.println("Ládával ütközik? (T/F)");
-						input = br.readLine();
-						if(input.equalsIgnoreCase("t"))
-						{
-							// Worker hit box
-							System.out.println("El tudja tolni? (T/F)");
-							input = br.readLine();
-							if(input.equalsIgnoreCase("t"))
-							{
-								// Worker move box
-								System.out.println("1. Másik doboznak tolja");
-								System.out.println("2. Munkásnak tolja");
-								System.out.println("3. Falnak tolja");
-								System.out.println("4. Lyukba tolja");
-								System.out.println("5. Darálóba tolja");
-								System.out.println("6. Kapcsolóra tolja");
-								input = br.readLine();
-								switch(input)
-								{
-								case "1":
-									System.out.println("Doboznak tolja");
-									// Worker move box hit box
-									break;
-								case "2":
-									System.out.println("Munkásnak tolja");
-									// Worker move box hit worker 
-									break;
-								case "3":
-									System.out.println("Falnak tolja");
-									// Worker move box hit wall
-									break;
-								case "4":
-									System.out.println("Lyukba tolja");
-									// Worker move box hit pit
-									break;
-								case "5":
-									System.out.println("Darálóba tolja");
-									// Worker move box hit grinder
-									System.out.println("Szerez pontot? (T/F)");
-									input = br.readLine();
-									if(input.equalsIgnoreCase("t"))
-									{
-										System.out.println("Pontot szerez");
-										// points++
-										
-										break;
-									}
-									if(input.equalsIgnoreCase("f"))
-									{
-										System.out.println("Nem szerez pontot");
-										break;
-									}
-									break;
-								case "6":
-									System.out.println("Kapcsolóra tolja");
-									// Worker move box hit switch
-									break;									
-								}
-							}
-							if(input.equalsIgnoreCase("f"))
-							{
-								System.out.println("Nem tudja eltolni");
-								break;
-							}
-						}
-						if(input.equalsIgnoreCase("f"))
-						{
-							System.out.println("Nem ütközik ládával");
-							break;
-						}				
-						break;
-					case "3":
-						System.out.println("Lyukkal ütközik? (T/F)");
-						input = br.readLine();
-						if(input.equalsIgnoreCase("t"))
-						{
-							//Worker hit pit
-							break;
-						}
-						if(input.equalsIgnoreCase("f"))
-						{
-							System.out.println("Nem ütközik lyukkal");
-							break;
-						}						
-						break;
-					case "4":
-						System.out.println("Munkással ütközik? (T/F)");
-						input = br.readLine();
-						if(input.equalsIgnoreCase("t"))
-						{
-							//Worker hit worker
-							break;
-						}
-						if(input.equalsIgnoreCase("f"))
-						{
-							System.out.println("Nem ütközik munnkással");
-							break;
-						}				
-						break;
-					case "5":
-						System.out.println("Kapcsolóval ütközik? (T/F)");
-						input = br.readLine();
-						if(input.equalsIgnoreCase("t"))
-						{
-							//Worker hit switch
-							
-							break;
-						}
-						if(input.equalsIgnoreCase("f"))
-						{
-							System.out.println("Nem ütközik kapcsolóval");
-							break;
-						}				
-						break;
-					default:
-						System.out.println("Hibás bemenet!");						
-					}
-				}
-				if(input.equalsIgnoreCase("f"))
-				{
-					System.out.println("Nem ütközik semmivel");
-					break;
-				}													
-				break;				
+				WorkerMoveMenu(input);
+				break;
 			case "3":
 				System.out.println("1. Játék vége");
 				System.out.println("2. Eredmények kijelzése");
 				System.out.println("3. Új játék");
 				input = br.readLine();
-				switch(input)
-				{
-				case "1":
-					System.out.println("Játék vége");
-					// Call EndGame
-					warehouse.EndGame();
-					break;
-				case "2":
-					System.out.println("Eredmények kijelzése");
-					// Show points
-					if(warehouse.GetWorkerNumber() > 1)
-					{
-						System.out.println("1. játékos pontjai: " + worker1.GetPoints());
-						System.out.println("2. játékos pontjai: " + worker2.GetPoints());
-						break;
-					}
-					else
-					{
-						System.out.println("1. játékos pontjai: " + worker1.GetPoints());						
-						break;
-					}
-				case "3":
-					System.out.println("Új játék");
-					// New game
-					break;
-				default:
-					System.out.println("Hibás bemenet!");
-				}								
+				EndGameMenu(input);
 				break;
 			case "4":
 				System.out.println("Kilépés");
