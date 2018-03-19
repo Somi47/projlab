0cab7a4e5d7525d226dad6b1425420568f5f2503
hollowfizz <hollowfizz@hotmail.hu>
2018-03-19 20:57:54 +0100

    fölösleges kód törlése

--- SkeletonApp/Program.java
+++ SkeletonApp/Program.java
@@ -492,26 +492,6 @@ public class Program
 			System.out.println("Hibás bemenet!");
 			nl(2);
 		}
-		
-		
-		/*System.out.println("Hali, ez egy példa a FunctionLogger működésére:");
-		
-		FunctionLogger.logFunctionCalled("calledName1", "calledFunction1()");
-			FunctionLogger.logFunctionCalled("calledName1/1", "calledFunction1/1()");
-			FunctionLogger.logFunctionReturnVoid();
-			FunctionLogger.logFunctionCalled("calledName1/2", "calledFunction1/2()");
-			FunctionLogger.logFunctionRetrun("Down");
-		FunctionLogger.logFunctionRetrun("Down");		
-		FunctionLogger.logFunctionCalled("calledName2", "calledFunction2()");
-			if( FunctionLogger.askUserDecision("Ládával ütközött?") ) {
-				FunctionLogger.logFunctionCalled("calledName2/Láda", "calledFunction2/Láda()");
-				FunctionLogger.logFunctionReturnVoid();
-			}
-			else {
-				FunctionLogger.logFunctionCalled("calledName2/NemLáda", "calledFunction2/NemLáda()");
-				FunctionLogger.logFunctionReturnVoid();
-			}
-		FunctionLogger.logFunctionReturnVoid();*/
-	}
-	
+					
+	}	
 }
