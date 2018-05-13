eff8620b8d4b85da00fc4e17dcb6e00a6b81bca5
Somi47 <extrackto@gmail.com>
2018-05-13 20:59:35 +0200

    duplikált fájlok törlése

--- GameLogic/GameLogic/Direction.java
+++
@@ -1,9 +0,0 @@
-package GameLogic;
-
-/* Enumeráció, mely a játék lehetséges irányit tárolja (fel, le, balra, jobbra) */
-public enum Direction {
-		Up,
-		Left,
-		Down,
-		Right;
-	}
--- GameLogic/GameLogic/Moveable.java
+++
@@ -1,7 +0,0 @@
-package GameLogic;
-
-/* Mozgatható dolgokat (doboz, munkás) reprezentáló interfész */
-public interface Moveable {
-	
-	public boolean Move(Direction dir, int force);
-}
